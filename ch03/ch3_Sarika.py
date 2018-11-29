378767b313288660f6726b20859d533ad8b753ad
Sarika Sarvia <sarika.sarvia1@gmail.com>
2018-11-29 16:39:07 +0000

    adding ch02 and ch03

---
+++ ch03/ch3_Sarika.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 29 13:58:25 2018
+
+@author: saras
+"""
+
+#print ("What's your name?")
+#name = input()
+#print ("Hello {}!".format(name))
+
+#print ("What's your name? ")
+#name = input()
+#name = input().upper()
+#print ("Hello {}!".format(name.upper()))
+#print ("Hello {}!".format(name.lower()))
+#print ("Hello {}!".format(name).title())
+#print ("Hello {}!".format(name))
+
+#name = input("What’s your name? ")
+#city = input("What’s your city? ")
+#print ("Hello {}! from {}".format(name,city))
+
+#age = input ('please type your age: ')
+
+"""def greeting():
+    print ("Hello World!")
+    
+#hello_world()
+    
+def introduction():
+    print ("What's your name? ")
+    name = input()
+    print ("Hello {}!".format(name.upper()))
+    greeting()
+    print (2+2)
+introduction()"""
+
+
+"""def hello_world():
+    print ("Hello World!")
+    
+def name_surname():
+    print ("What's your name?")
+    name = input()
+    
+    print ("My name is {}".format(name))
+    addition()
+    
+def addition():
+    add2_2 = 2+2
+    print (add2_2)
+    
+hello_world()
+name_surname()"""
+
+def asking():
+    print ("Please choose a number")
+    numbera = input()
+    print ("Please choose another number")
+    numberb = input()
+    print (int(numbera)+int(numberb))
+asking()
+
+
+     
