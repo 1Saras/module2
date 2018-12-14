16c97d3903da323a78fd10aa2f23be9209372c1c
Sarika Sarvia <sarika.sarvia1@gmail.com>
2018-12-14 16:20:41 +0000

    added to metals.py

--- ch10/metals.py
+++ ch10/metals.py
@@ -8,11 +8,18 @@ Created on Fri Dec 14 14:09:04 2018
 metals = {'iron':(7.8, 12.5, 498), 'gold':(19.3, 32.1, 230), 'zinc':(7.13, 84.8, 879), 'lead':(11.4, 98.1, 384)}
 print(metals)
 
+#turns the tuples into lists#
 metals_keys = list(metals.keys())
-
-print(sorted(metals.items(), key=lambda kv:kv[1][1]))
-
-print(sorted(metals.items(), key=lambda kv:kv[1][1],reverse=True))
-
-metals_keys.sort(key=lambda k:metals[k][1],reverse=True)
-print (metals_keys)
+#
+##sorts by share price accending#
+#print(sorted(metals.items(), key=lambda kv:kv[1][1]))
+#
+##sorts by share price in reverse order#
+#print(sorted(metals.items(), key=lambda kv:kv[1][1],reverse=True))
+#
+##sorts by share price in reverse order but only showing the keys#
+#metals_keys.sort(key=lambda k:metals[k][1],reverse=True)
+#print(metals_keys)
+#
+#sorting by experiment ascending#
+#print(sorted(metals.items(), key=lambda kv:kv[1][2]))
