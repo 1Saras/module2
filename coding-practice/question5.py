81d159daa3604f3280425f6ab42d32c9b197106d
Sarika Sarvia <sarika.sarvia1@gmail.com>
2019-01-08 16:13:00 +0000

    amended coding practice

--- coding-practice/question5.py
+++ coding-practice/question5.py
@@ -9,13 +9,15 @@ import string
 
 #print(string.ascii_uppercase)
 #
-##Task 1
+##Task 1 -Write the letters 'A' to 'Z' (in upper case) to the console, placing each letter on a new line.
+
 #for char in string.ascii_uppercase:
 #    print(char)
 
 
-#Task 2
-alphabet = string.ascii_uppercase
+#Task 2 -For every third letter, write it to the console in lowercase instead.
+
+#alphabet = string.ascii_uppercase
 #indexs = range(0,len(alphabet))
 #def count_three(alphabet):
 #    letter = -1
@@ -24,14 +26,29 @@ alphabet = string.ascii_uppercase
 #        if letter +=3:
 #            print(alphabet.lower())
             
-            
-for i in range(0,len(alphabet)):
-    if i %3==2:
-        print(alphabet[i].lower())    
-    else:
-        print (alphabet[i])
-#    
 #    print ('index',i,'with',alphabet[i] )
     
-    
+#Task 2 - improved
 
+#alphabet = string.ascii_uppercase
+#for i in range(0,len(alphabet)):
+#    if i %3==2:
+#        print(alphabet[i].lower())    
+#    else:
+#        print (alphabet[i])
+        
+#Task 3 - For every fourth letter, write its numeric position in the alphabet to the console instead (e.g. instead of outputting 'D' output '4').
+
+#alphabet = string.ascii_uppercase
+#for i in range(0,len(alphabet)):
+#    if i %4==3:
+#        print(i+1)    
+#    else:
+#        print (alphabet[i])    
+        
+#Task 4 - If a letter satisfies both rules 2 and 3, write out its numeric position followed by the letter in lowercase (e.g. 'L' should be output as '12l').
+
+alphabet = string.ascii_uppercase
+for i in range(0,len(alphabet)):
+    if i %3==2 or i %4==3:
+        print((i+1) + (alphabet[i].lower()))
